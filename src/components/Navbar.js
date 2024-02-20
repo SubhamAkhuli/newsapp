9cdcf9765b500f424d2b500b46790ac2c9e09aa7
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-02-20 10:39:14 +0530

    Remove search form from Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,10 +33,10 @@ export class Navbar extends Component {
                                     </ul>
                                 </li>
                             </ul>
-                            <form className="d-flex" role="search">
+                            {/* <form className="d-flex" role="search">
                                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                                 <button className="btn btn-outline-success" type="submit">Search</button>
-                            </form>
+                            </form> */}
                         </div>
                     </div>
                 </nav>
