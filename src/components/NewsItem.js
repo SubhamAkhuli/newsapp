fcb5e28e3bad59afa740aa913dd5bf8abc62229c
SubhamAkhuli <subhamakhuli7586@gmail.com>
2024-02-23 13:25:23 +0530

    Update image URL in NewsItem component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, imageUrl, newsUrl, author, date ,source} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div>
                 <div className="card" >
-                    <img src={!imageUrl ? "https://static.toiimg.com/thumb/msid-107761035,width-1070,height-580,imgsize-866210,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <img src={!imageUrl ? "https://c.ndtvimg.com/2024-02/ut2p2a1o_lake-generic-unsplash_625x300_20_February_24.jpeg?ver-20240117.06" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{left:"90%", zIndex:"1"}}>
+                        <h5 className="card-title">{title}...<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{ left: "90%", zIndex: "1" }}>
                             {source}
                         </span><span className='badge bg-secondary'>New</span></h5>
                         <p className="card-text">{description}...</p>
